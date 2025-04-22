2ad826cd5f08a12b3056ed541c8475b5c27d473c
hasan110022 <unitaxtours@gmail.com>
2025-04-22 11:46:28 +0600

    bookings

--- index.js
+++ index.js
@@ -30,21 +30,67 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const tourCollection = client.db('tourDB').collection('tour');
-    const destinationCollection = client.db('destinationDB').collection('destination');
+    const tourCollection = client.db('tourdb').collection('tour');
+    const destinationCollection = client.db('tourdb').collection('destination');
+    const usersCollections = client.db('tourdb').collection('users');
+    const packagesCollections = client.db('tourdb').collection('packages');
+    const bookingsCollections = client.db('tourdb').collection('bookings');
 
-    app.get('/tour', async(req,res) =>{
-        const cursor = tourCollection.find();
+    app.get('/packages', async(req,res) =>{
+        const cursor = packagesCollections.find();
         const result = await cursor.toArray();
         res.send(result);
     });
+    app.post('/packages', async(req,res) =>{
+      const newTour = req.body;
+      const result = await packagesCollections.insertOne(newTour);
+      res.send(result);
+  });
 
-    app.get('/tour/:id', async(req,res)=>{
+    app.get('/bookings', async(req,res) =>{
+        const cursor = bookingsCollections.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    });
+    app.post('/bookings', async(req,res) =>{
+      const newTour = req.body;
+      const result = await bookingsCollections.insertOne(newTour);
+      res.send(result);
+  });
+
+    app.post('/users/:email', async (req, res) => {
+      const newuser = req.body;
+      const email = req.params.email
+      const query = { email }
+      const existingUser = await usersCollections.findOne(query)
+      if (existingUser) {
+        return res.send('existing User')
+      }
+      const result = await usersCollections.insertOne({
+        ...newuser,
+        timeStamp: Date.now(),
+      })
+      res.send(result)
+    })
+    app.get('/users/:email', async (req, res) => {
+      const email = req.params.email
+      const query = { email }
+      const result = await usersCollections.findOne(query)
+      res.send(result)
+    })
+
+    app.get('/packages/:id', async(req,res)=>{
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
-      const result = await tourCollection.findOne(query);
+      const result = await packagesCollections.findOne(query);
       res.send(result);
     });
+    app.delete('/packages/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await packagesCollections.deleteOne(query)
+      res.send(result)
+    })
 
     // destination
 
@@ -63,12 +109,6 @@ async function run() {
     });
 
 
-    app.post('/tour', async(req,res) =>{
-        const newTour = req.body;
-        console.log(newTour);
-        const result = await tourCollection.insertOne(newTour);
-        res.send(result);
-    });
 
     app.post('/destination', async(req,res) =>{
       const newDestination = req.body;
